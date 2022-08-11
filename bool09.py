2e861aa2ebe13c69e09f7abb914fff9c1f2d1eba
Samandar <samandarnfh.2020@gmail.com>
2022-08-11 14:28:03 +0500

    fg

--- bool09.py
+++ bool09.py
@@ -6,5 +6,8 @@ def main(a):
         bool
     """
     # Write your code here
-    return a>0
-print(main(-8))
+    x1 = int(a > 0)
+    x2 = int (a%1 == 0)
+
+    return x1 + x2 > 1
+print(main(5)) 
